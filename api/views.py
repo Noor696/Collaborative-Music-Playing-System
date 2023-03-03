032b4c0ab58cf377e89e14934dac0be63ba7579f
Noor696 <eng.nooralhateeb@gmail.com>
2023-03-03 18:25:10 +0000

    update view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from rest_framework import generics  # this allows to make class inhiret from API view
+from .models import Room
+from .serializers import RoomSerializer
 # Create your views here.
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+
+class RoomView(generics.CreateAPIView):
+    # Allow us to view all the different model (Rooms) and Create A Room
+    queryset = Room.objects.all() # tell the queryset what do we want to return / take all of room object and give him to serilizer
+    serializer_class = RoomSerializer  # a file resposible to convert python code to JSON data/ format
