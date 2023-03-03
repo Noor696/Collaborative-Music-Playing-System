42f3fb173a8f9b29dee92c369ba5ae111b364a0a
Noor696 <eng.nooralhateeb@gmail.com>
2023-03-03 19:02:10 +0000

    Update URL

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from .models import Room
 from .serializers import RoomSerializer
 # Create your views here.
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     # Allow us to view all the different model (Rooms) and Create A Room
     queryset = Room.objects.all() # tell the queryset what do we want to return / take all of room object and give him to serilizer
     serializer_class = RoomSerializer  # a file resposible to convert python code to JSON data/ format
